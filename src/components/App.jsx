de0289244b0dfe6a1559d13e943b73981b17b57d
NataliiaChapai <natalya.chapay@gmail.com>
2022-05-20 20:51:07 +0300

    rewrite through hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,15 +15,15 @@ export default function App() {
     
     switch (name) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
         break;
       
       case 'bad':
-        setBad(bad + 1);
+        setBad(prev => prev + 1);
         break;
       
       default:
